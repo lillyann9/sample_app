f86463b240fce2745a3c22b6f39c29b2265214b4
lillyann9 <lilly.0608@gmail.com>
2015-10-11 16:43:32 -0500

    Add user microposts

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
-  
+   has_many :microposts, dependent: :destroy
+
   attr_accessor :remember_token, :activation_token, :reset_token
   attr_accessor :remember_token, :activation_token
   before_save   :downcase_email
@@ -87,7 +88,11 @@ class User < ActiveRecord::Base
   def password_reset_expired?
     reset_sent_at < 2.hours.ago
   end
-
+  def feed
+  # This is preliminary. See "Following users" for the full implementation.
+  Todo.where("user_id = ?", id)
+end
+  
   private
 
     # Converts email to all lower-case.
@@ -100,4 +105,6 @@ class User < ActiveRecord::Base
       self.activation_token  = User.new_token
       self.activation_digest = User.digest(activation_token)
     end
+
+
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -6,8 +6,6 @@ class UserTest < ActiveSupport::TestCase
     @user = User.new(name: "Example User", email: "user@example.com",
                      password: "foobar", password_confirmation: "foobar")
   end
- 
-
   test "should be valid" do
     assert @user.valid?
   end
@@ -62,6 +60,13 @@ class UserTest < ActiveSupport::TestCase
   end
   test "authenticated? should return false for a user with nil digest" do
     assert_not @user.authenticated?('')
+  end
+   test "associated microposts should be destroyed" do
+    @user.save
+    @user.microposts.create!(content: "Lorem ipsum")
+    assert_difference 'Micropost.count', -1 do
+      @user.destroy
+    end
   end
 
 end
